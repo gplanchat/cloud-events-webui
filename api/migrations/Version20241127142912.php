e0186d6308ddc4961029da8ddba08a99e5583f1d
Grégory Planchat <gregory@gyroscops.com>
2024-11-27 15:29:32 +0100

    Fixed migrations

---
+++ api/migrations/Version20241127142912.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20241127142912 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE subscriber ADD label VARCHAR(255) NOT NULL');
+        $this->addSql('CREATE INDEX IDX_AD005B69EA750E8 ON subscriber (label)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('DROP INDEX IDX_AD005B69EA750E8');
+        $this->addSql('ALTER TABLE subscriber DROP label');
+    }
+}
