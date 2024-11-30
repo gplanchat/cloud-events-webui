74d27293d99210bbd4f6219247b22f6b4267e5ce
Grégory Planchat <gregory@gyroscops.com>
2024-11-30 18:27:03 +0100

    Added new fields on subscriber to add code

--- pwa/components/admin/subscribers/SubscribersEdit.tsx
+++ pwa/components/admin/subscribers/SubscribersEdit.tsx
@@ -5,6 +5,8 @@ const SubscribersEdit = () => (
   <Edit>
     <SimpleForm>
       <TextInput name="label" source="label" />
+      <TextInput name="description" source="description" />
+      <TextInput name="code" source="code" readOnly />
       <TextInput name="serviceUri" source="serviceUri" />
       <BooleanInput name="verifyPeer" source="verifyPeer" helperText="Disable TLS verification for this subscriber. In case it is using a self-signed certificate you should disable the peer verification." />
       <TriggerFilterInput name="filters" source="filters" />
